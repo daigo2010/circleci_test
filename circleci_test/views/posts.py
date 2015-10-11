bb7efb1310b52cc41320aaed40649cdd4529ade1
daigo <dragonmind2002@yahoo.co.jp>
2015-10-12 08:58:19 +0900

    first commit

---
+++ circleci_test/views/posts.py
@@ -0,0 +1,20 @@
+from django.template import Context, loader, RequestContext
+from circleci_test.models import Posts
+from django.http import HttpResponse
+from datetime import datetime
+
+def index(request):
+    if request.method == "POST":
+        p = Posts(name=request.POST['name'], description=request.POST['description'],url=request.POST['url'],  created_at=datetime.now())
+        p.save()
+    posts = Posts.objects.all().order_by('-created_at')[:5]
+    t = loader.get_template('./index.html')
+    c = Context({
+        'posts': posts,
+    })
+    c = RequestContext(request, {
+        'posts': posts,
+    })
+
+
+    return HttpResponse(t.render(c))
