250f06074e627b08b5903edba37d4c1479e61185
daigo <dragonmind2002@yahoo.co.jp>
2015-10-12 10:28:28 +0900

    real test

--- circleci_test/models.py
+++ circleci_test/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
+class PostsManager(models.Manager):
+    def get_recent_data(self, count=5):
+        return super(PostsManager, self).all().order_by('-created_at')[:count]
+
 class Posts(models.Model):
     name = models.CharField(max_length=30)
     description = models.TextField()
     url = models.TextField()
     created_at = models.DateTimeField()
 
-    def get_recent_data(count=5):
-        return self.objects.all().order_by('-created_at')[:count]
+    objects = PostsManager()
--- circleci_test/tests/posts.py
+++ circleci_test/tests/posts.py
@@ -8,4 +8,11 @@ class PostsModelTestCase(unittest.TestCase):
             p = Posts(name="name{0}".format(i), description="description{0}".format(i),url="url{0}".format(i),  created_at=datetime.now())
             p.save()
 
-        self.assertEquals(1, 1)
+        posts = Posts.objects.get_recent_data()
+
+        self.assertEquals(len(posts), 5)
+        self.assertEquals(posts[0].name, "name5")
+        self.assertEquals(posts[1].name, "name4")
+        self.assertEquals(posts[2].name, "name3")
+        self.assertEquals(posts[3].name, "name2")
+        self.assertEquals(posts[4].name, "name1")
