12843c6e3cb5607b25cf7cb52da7cc934cd57ef2
daigo <dragonmind2002@yahoo.co.jp>
2015-10-12 09:57:44 +0900

    add test

---
+++ circleci_test/tests/posts.py
@@ -0,0 +1,11 @@
+import unittest
+from datetime import datetime
+from circleci_test.models import Posts
+
+class PostsModelTestCase(unittest.TestCase):
+    def test_get_recent_data(self):
+        for i in range(0, 6):
+            p = Posts(name="name{0}".format(i), description="description{0}".format(i),url="url{0}".format(i),  created_at=datetime.now())
+            p.save()
+
+        self.assertEquals(1, 0)
